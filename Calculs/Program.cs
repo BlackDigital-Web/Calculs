7fc842990d9a7e40e7115f8fae7ac870a10e520f
CLAIRE Whitney <CLAIRE Whitney@LAPTOP-FS4TB79A>
2021-10-13 22:34:10 +0200

    Ajoutez des fichiers projet.

---
+++ Calculs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
